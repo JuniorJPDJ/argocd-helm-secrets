2e7a4859721cc87f3a2e12db2345697d868e333b
JuniorJPDJ <git@juniorjpdj.pl>
2024-03-18 00:39:28 +0100

    fix(helm-vault-k8s-auth-wrapper): mount path variable should contain only the auth method mount path

--- helm-vault-k8s-auth-wrapper.sh
+++ helm-vault-k8s-auth-wrapper.sh
@@ -16,7 +16,7 @@ done
 
 if [[ $DECODING_SECRETS = 1 ]] ; then
   # https://developer.hashicorp.com/vault/api-docs/auth/kubernetes#login
-  export VAULT_TOKEN="$(curl -s -X POST -d '{"role": "'"$VAULT_K8S_ROLE"'", "jwt": "'"$K8S_SA_TOKEN"'"}' "$VAULT_ADDR/v1/${VAULT_K8S_MOUNT_PATH:-auth/kubernetes/login}" | jq -r '.auth.client_token | select( . != null )')"
+  export VAULT_TOKEN="$(curl -s -X POST -d '{"role": "'"$VAULT_K8S_ROLE"'", "jwt": "'"$K8S_SA_TOKEN"'"}' "$VAULT_ADDR/v1/auth/${VAULT_K8S_MOUNT_PATH:-kubernetes}/login" | jq -r '.auth.client_token | select( . != null )')"
   # if auth failed this should be empty
 fi
 
